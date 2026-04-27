23c39cf3e5031d8d4795ff217408ceb334cf924c
Priyanka <150278784+Priyankakalsi@users.noreply.github.com>
2026-04-27 23:22:55 +0530

    prime-check.cpp
    Fix formatting and clean up whitespace in prime-check.cpp

--- 1. prime-check.cpp
+++ 1. prime-check.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 bool is_prime(int);
-
 int main()
 {
     int num;
-
     cout << "Enter number to check: ";
     cin >> num;
-
     if (is_prime(num))
     {
         cout << num << " is Prime";
@@ -19,10 +14,8 @@ int main()
     {
         cout << num << " is Not Prime";
     }
-
     return 0;
 }
-
 bool is_prime(int num)
 {
     for (int i = 2; i < num / 2; i++)
